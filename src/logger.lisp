eef729fa88505ec078ce0889092c097506482e8e
Johnny Ruiz <jruiz@mini.local>
2020-03-25 21:44:16 -0400

    Fix daily rolling file logger configuration

--- src/logger.lisp
+++ src/logger.lisp
@@ -1,9 +1,10 @@
 ;;;; Logger component definition
 
 (in-package #:zledger)
+
 (declaim #.*compile-declaration*)
 
 (defcomponent logger ()
   (:start
-   (when **logger-daily-file*
+   (when *logger-daily-file*
      (log:config :daily *daily-logger-file*))))
